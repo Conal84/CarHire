3a97d14ad1856636748809b93d99eda282539105
Conal84 <conal.walsh1@gmail.com>
2020-05-17 12:46:30 +0000

    Added default route and tested functionality

--- app.py
+++ app.py
@@ -12,3 +12,12 @@ app.config['MONGO_DBNAME'] = 'CarHire'
 app.config['MONGO_URI'] = os.environ.get('MONGO_URI')
 
 mongo = PyMongo(app)
+
+@app.route('/')
+def hello():
+    return "Hello world!!!"
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+        port=int(os.environ.get('PORT')),
+        debug=True)
